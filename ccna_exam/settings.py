5107b8a1f572a8b227310289b5f7a0e3c80b14d9
David Jose  Manzano Arias <davidjosemanzano@gmail.com>
2025-08-21 09:09:42 -0500

    Carga a server

--- ccna_exam/settings.py
+++ ccna_exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7**qyjjk%bv#zm7&3!gz*fp91^fpz)3m1@i^3@vwpdbcp6g#l^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
